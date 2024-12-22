1c536b2b82457c3187c92d5e92189da7725520fb
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2024-12-22 14:30:04 -0300

    fix: fixes the bundling of the obsidian package

--- new/packages/common/vite.config.ts
+++ new/packages/common/vite.config.ts
@@ -6,6 +6,9 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
 	build: {
 		lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] },
+		rollupOptions: {
+			external: ['obsidian'],
+		},
 	},
 	resolve: { alias: { src: resolve('src/') } },
 	plugins: [dts()],
